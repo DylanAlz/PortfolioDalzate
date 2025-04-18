ac2f244cc57213aa97eddaf07c7dc5ba12662d7e
DYLAN STYV ALZATE BETANCUR <dylanalzate300037@correo.itm.edu.co>
2025-04-18 00:33:59 -0500

    project creation

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import Menubar from './Components/MenuBar'
+import React from 'react'
+import './App.css'
+
+const App: React.FC = () => {
+  return ( 
+    <>
+    <Menubar/> 
+      <div>
+        <p>Hola</p>
+      </div> 
+    
+    </> 
+  )
+}
+
+export default App
---
+++ src/Components/MenuBar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Menubar: React.FC = () => {
+return (
+      <header>
+        <nav>
+          <ul>
+            <li>hola</li>
+            <li>hola2</li>
+            <li>hola3</li>
+            <li></li>
+          </ul>
+        </nav>
+      </header>
+)
+}
+
+export default Menubar;
